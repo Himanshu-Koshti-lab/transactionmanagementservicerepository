57b0b12c0f4c1424829562c43cc2f1e04e8a2c47
rkrohitkakade <kakade2506@gmail.com>
2020-11-18 10:50:49 +0530

    Transaction management Eureka client

--- src/main/java/com/tcs/poc/app/TransactionManagementServiceApplication.java
+++ src/main/java/com/tcs/poc/app/TransactionManagementServiceApplication.java
@@ -2,7 +2,9 @@ package com.tcs.poc.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class TransactionManagementServiceApplication {
 

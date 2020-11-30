c7860aaa255cd89b3343cada98a833595ecbb896
Himanshu Koshti <Himfeedback74@gmail.com>
2020-11-30 22:54:13 +0530

    Same Resource

--- src/main/java/com/tcs/poc/app/config/ResourceServerConfig.java
+++ src/main/java/com/tcs/poc/app/config/ResourceServerConfig.java
@@ -1,6 +1,5 @@
 package com.tcs.poc.app.config;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -12,40 +11,23 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
-	
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
 	@Autowired
 	private Environment env;
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.resourceId(env.getProperty("security.oauth2.client.clientId"));
-		
+
 	}
-	
+
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-//		http
-//			.csrf()
-//				.disable()
-//			.formLogin()
-//				.disable()
-//			.httpBasic()
-//				.disable()
-//			.sessionManagement()
-//			.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-//			.and()
-//			.authorizeRequests()
-//			.anyRequest()
-//			.authenticated();
 		http
 		.csrf().disable()
-		.authorizeRequests().antMatchers("/oauth/token","/login","/service/register-user","/forgotPasswordByQuestion","/forgotPasswordByOtp","/VerifyOtp").permitAll()
-//		.antMatchers("/Admin").hasRole("ADMIN")
-//		.antMatchers("/User").hasRole("USER")
-		.antMatchers("/Admin").hasRole("ADMIN")
-		.antMatchers("/sendMoney").hasRole("CUSTOMER")
-		.antMatchers("/Employee").hasRole("EMPLOYEE")
+		.authorizeRequests()
+		.antMatchers("/**").permitAll()
 		.anyRequest().authenticated()
 		.and()
 		.cors();
